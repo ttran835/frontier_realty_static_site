a17acafea0eab7979adb0595305e0f3cb73287b3
datusername1 <timothy.tran835@gmail.com>
2019-02-15 04:18:53 -0800

    completed team carousel, but only able to implement 4x1 and 1x1, will try to figure out away to render 4x4

--- frontier_static/js/slick.js
+++ frontier_static/js/slick.js
@@ -26,4 +26,34 @@ $(document).ready(function() {
       },
     ],
   });
+  $('#team-carousel').slick({
+    centerPadding: '60px',
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  });
 });
